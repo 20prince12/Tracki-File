5fcd8b0abdbc41fe1c3fc296740beb13fe6f216f
Prince Patel <felicita.cse@gmail.com>
2021-05-27 19:04:16 +0530

    v0.1

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask,request
+from flask.templating import render_template
+import json
+import requests
+
+
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    ip=request.args.get('ip')
+    print(ip)
+    url = f'http://ipinfo.io/{ip}?token=91ad2d6d618ec3'
+    response = requests.get(url=url)
+    data = response.json()
+    print(data)
+    return "<script>window.close()</script>"
 
 
 if __name__ == '__main__':
